4e88c1ea074168f9c1ba8d5d2948741dba090a83
Calebe Santos <calebe.bsantos@outlook.com>
2018-08-20 20:00:52 -0300

    Replace temp with query
    Long method - O método statement foi reduzido porém ainda havia variáveis temporárias
    que poderiam ser substituidos por queries.
    
    Assim reduzindo ainda mais o tamanho do método.

--- src/br/edu/infnet/edc/calebesantos/tp1/api/Customer.java
+++ src/br/edu/infnet/edc/calebesantos/tp1/api/Customer.java
@@ -2,6 +2,7 @@ package br.edu.infnet.edc.calebesantos.tp1.api;
 
 import java.util.Enumeration;
 import java.util.Vector;
+import java.util.stream.Stream;
 
 import br.edu.infnet.edc.calebesantos.tp1.models.Rental;
 
@@ -22,29 +23,38 @@ public class Customer {
 	}
 
 	public String statement() {
-		double totalAmount = 0;
-		int frequentRenterPoints = 0;
+		double totalAmount = getTotalAmount();
+		int frequentRenterPoints = getFrequentRenterPoints();
 		Enumeration<Rental> rentals = _rentals.elements();
+
 		String result = generateHeader();
 
 		while (rentals.hasMoreElements()) {
 			Rental each = (Rental) rentals.nextElement();
 
-			// determine amounts for each line
-			double thisAmount = each.calculateAmount();
-
 			// add frequent renter points
 			frequentRenterPoints += each.calculateFrequentRenterPoints();
 
 			// show figures for this rental
-			result = generateFigureForRental(result, each, thisAmount);
-			totalAmount += thisAmount;
+			result = generateFigureForRental(each);
 		}
 
 		// add footer lines
-		result = generateFooter(totalAmount, frequentRenterPoints, result);
+		result = generateFooter(totalAmount, frequentRenterPoints);
 
 		return result;
+	}
+
+	private int getFrequentRenterPoints() {
+		return getRentalsStream().mapToInt(r -> r.calculateFrequentRenterPoints()).sum();
+	}
+
+	private double getTotalAmount() {
+		return getRentalsStream().mapToDouble(r -> r.calculateAmount()).sum();
+	}
+
+	private Stream<Rental> getRentalsStream() {
+		return _rentals.stream();
 	}
 
 	private String generateHeader() {
@@ -52,14 +62,12 @@ public class Customer {
 		return result;
 	}
 
-	private String generateFigureForRental(String result, Rental each, double thisAmount) {
-		result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-		return result;
+	private String generateFigureForRental(Rental each) {
+		return "\t" + each.getMovie().getTitle() + "\t" + each.calculateAmount() + "\n";
 	}
 
-	private String generateFooter(double totalAmount, int frequentRenterPoints, String result) {
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
-		return result;
+	private String generateFooter(double totalAmount, int frequentRenterPoints) {
+		return "Amount owed is " + String.valueOf(totalAmount) + "\n" + "You earned "
+				+ String.valueOf(frequentRenterPoints) + " frequent renter points";
 	}
 }
