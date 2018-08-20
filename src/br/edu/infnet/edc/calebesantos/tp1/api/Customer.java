207db0036a18bbc283a89eca5015e2c3052b1d8f
Calebe Santos <calebe.bsantos@outlook.com>
2018-08-20 19:47:30 -0300

    Move method
    Feature envy - A classe customer estava acessando mais os dados de outra classe para
    realizar operações do que as próprias classes
    
    Para isso foi movido os métodos para suas respectivas classes

--- src/br/edu/infnet/edc/calebesantos/tp1/api/Customer.java
+++ src/br/edu/infnet/edc/calebesantos/tp1/api/Customer.java
@@ -3,65 +3,48 @@ package br.edu.infnet.edc.calebesantos.tp1.api;
 import java.util.Enumeration;
 import java.util.Vector;
 
-import br.edu.infnet.edc.calebesantos.tp1.models.Movie;
 import br.edu.infnet.edc.calebesantos.tp1.models.Rental;
 
 public class Customer {
-    private String _name;
-    private Vector<Rental> _rentals = new Vector<>();
+	private String _name;
+	private Vector<Rental> _rentals = new Vector<>();
 
-    public Customer(String name) {
-        _name = name;
-    }
-
-    public void addRental(Rental arg) {
-        _rentals.addElement(arg);
-    }
-
-    public String getName() {
-        return _name;
-    }
-
-    public String statement() {
-        double totalAmount = 0;
-        int frequentRenterPoints = 0;
-        Enumeration<Rental> rentals = _rentals.elements();
-        String result = generateHeader();
-
-        while (rentals.hasMoreElements()) {
-            Rental each = (Rental) rentals.nextElement();
-
-            // determine amounts for each line
-            double thisAmount = calculateAmount(each);
-
-            // add frequent renter points
-            frequentRenterPoints += calculateFrequentRenterPoints(frequentRenterPoints, each);
-
-            // show figures for this rental
-            result = generateFigureForRental(result, each, thisAmount);
-            totalAmount += thisAmount;
-        }
-
-        // add footer lines
-        result = generateFooter(totalAmount, frequentRenterPoints, result);
-
-        return result;
-    }
-
-	private String generateFigureForRental(String result, Rental each, double thisAmount) {
-		result += "\t" + each.getMovie().getTitle() + "\t" +
-		        String.valueOf(thisAmount) + "\n";
-		return result;
+	public Customer(String name) {
+		_name = name;
 	}
 
-	private int calculateFrequentRenterPoints(int initialFrequentRenterPoints, Rental each) {
-		int frequentRenterPoints = initialFrequentRenterPoints + 1;
+	public void addRental(Rental arg) {
+		_rentals.addElement(arg);
+	}
 
-		// add bonus for a two day new release rental
-		if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-		        each.getDaysRented() > 1) frequentRenterPoints ++;
+	public String getName() {
+		return _name;
+	}
 
-		return frequentRenterPoints;
+	public String statement() {
+		double totalAmount = 0;
+		int frequentRenterPoints = 0;
+		Enumeration<Rental> rentals = _rentals.elements();
+		String result = generateHeader();
+
+		while (rentals.hasMoreElements()) {
+			Rental each = (Rental) rentals.nextElement();
+
+			// determine amounts for each line
+			double thisAmount = each.calculateAmount();
+
+			// add frequent renter points
+			frequentRenterPoints += each.calculateFrequentRenterPoints();
+
+			// show figures for this rental
+			result = generateFigureForRental(result, each, thisAmount);
+			totalAmount += thisAmount;
+		}
+
+		// add footer lines
+		result = generateFooter(totalAmount, frequentRenterPoints, result);
+
+		return result;
 	}
 
 	private String generateHeader() {
@@ -69,30 +52,14 @@ public class Customer {
 		return result;
 	}
 
-	private String generateFooter(double totalAmount, int frequentRenterPoints, String result) {
-		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) +
-                " frequent renter points";
+	private String generateFigureForRental(String result, Rental each, double thisAmount) {
+		result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
 		return result;
 	}
 
-	private double calculateAmount(Rental each) {
-        double thisAmount = 0;
-		switch (each.getMovie().getPriceCode()) {
-		    case Movie.REGULAR:
-		        thisAmount += 2;
-		        if (each.getDaysRented() > 2)
-		            thisAmount += (each.getDaysRented() - 2) * 1.5;
-		        break;
-		    case Movie.NEW_RELEASE:
-		        thisAmount += each.getDaysRented() * 3;
-		        break;
-		    case  Movie.CHILDRENS:
-		        thisAmount += 1.5;
-		        if (each.getDaysRented() > 3)
-		            thisAmount += (each.getDaysRented() - 3) * 1.5;
-		        break;
-		}
-		return thisAmount;
+	private String generateFooter(double totalAmount, int frequentRenterPoints, String result) {
+		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
+		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+		return result;
 	}
 }
